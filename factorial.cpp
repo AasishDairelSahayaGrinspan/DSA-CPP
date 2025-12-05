0daed4bb5e1e9445d14e0a6529627f7b151dee4f
AasishDairelSahayaGrinspan <aasishdairel@gmail.com>
2025-12-06 01:54:01 +0530

    dec 6

--- factorial.cpp
+++ factorial.cpp
@@ -10,6 +10,16 @@ using namespace std;
 
         }
         return ans;
+
+    }
+    //for loop version
+        int fac(int n) {
+        int ans = 1;
+        for (int i = 1; i<= n; i++ ) {
+            ans = ans * i;
+
+        }
+            return ans;
     }
 
 int main() {
