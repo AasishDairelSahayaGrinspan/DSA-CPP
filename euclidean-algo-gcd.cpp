7c484c763472ef7974d540dfe1530c2c0350b5a8
AasishDairelSahayaGrinspan <aasishdairel@gmail.com>
2025-12-08 00:39:18 +0530

    dec 8

---
+++ euclidean-algo-gcd.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <numeric>
+using namespace std;
+
+int gcd(int a , int b ) {
+    while(a!=b) {
+        if (a>b) {
+            a = a-b;
+        }
+
+        else  {
+            b = b-a;
+        }
+
+
+    }
+    return a;
+}
+
+int main () {
+
+    int a , b;
+    cout<<"enter the first number:";
+    cin>>a>>b;
+    cout<<gcd(a,b)<<endl;
+
+}
